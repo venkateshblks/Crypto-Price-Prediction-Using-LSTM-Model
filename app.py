9b92116d42d42f24cfb7df0d0bb9d14936d3f1ba
Venkateswararao <87414605+venkateshblks@users.noreply.github.com>
2024-10-06 09:54:08 +0000

    updated

--- app.py
+++ app.py
@@ -26,22 +26,11 @@ def fetch_data(symbol):
     data['Datetime'] = data['Datetime'] + pd.Timedelta(hours=5, minutes=30)
     return data.drop(['Adj Close', 'Volume'], axis=1)
 
-def create_features(data):
-    for lag in range(1, 6):
-        data[f'Close_Lag_{lag}'] = data['Close'].shift(lag)
-    data['SMA_5'] = data['Close'].rolling(window=5).mean()
-    data['SMA_10'] = data['Close'].rolling(window=10).mean()
-    data['EMA_5'] = data['Close'].ewm(span=5, adjust=False).mean()
-    data['Volatility'] = data['Close'].rolling(window=5).std()
-    data['Momentum_5'] = data['Close'] - data['Close'].shift(5)
-    data['Pct_Change'] = data['Close'].pct_change()
-    data.dropna(inplace=True)
-    return data
 
 def predict_prophet(data):
     model = Prophet()
     model.fit(data)
-    future = model.make_future_dataframe(periods=120, freq='1h')
+    future = model.make_future_dataframe(periods=24, freq='1h')
     return model.predict(future)
 def create_prophet_plot(symbol):
     data = fetch_data(symbol)
@@ -52,7 +41,7 @@ def create_prophet_plot(symbol):
     
     prophet_forecast = predict_prophet(df_prophet)
     df_prophet = df_prophet[df_prophet['ds'] >= (df_prophet['ds'].max() - timedelta(days=30))]
-    prophet_forecast = prophet_forecast[prophet_forecast['ds'] >= (prophet_forecast['ds'].max() - timedelta(days=35))]
+    prophet_forecast = prophet_forecast[prophet_forecast['ds'] >= (prophet_forecast['ds'].max() - timedelta(days=31))]
 
     prophet_fig = go.Figure()
     prophet_fig.add_trace(go.Scatter(x=df_prophet['ds'], y=df_prophet['y'], mode='lines', name='Historical Prices'))
@@ -69,11 +58,9 @@ def create_dataset(data, time_step=1):
 def training_model(data):
     scaler = MinMaxScaler(feature_range=(0, 1))
     data['Close'] = scaler.fit_transform(data[['Close']])
-    for lag in range(1, 6):
-        data[f'Close_Lag_{lag}'] = data['Close'].shift(lag)
-    data.dropna(inplace=True)
     dataset = data['Close'].values.reshape(-1, 1)
-    X = create_dataset(dataset, time_step=10)
+    ts=24
+    X = create_dataset(dataset, time_step=ts)
     X = X.reshape(X.shape[0], X.shape[1], 1)
     model = Sequential()
     model.add(LSTM(50, return_sequences=True, input_shape=(X.shape[1], 1)))
@@ -82,13 +69,13 @@ def training_model(data):
     model.add(Dropout(0.2))
     model.add(Dense(1))
     model.compile(optimizer='adam', loss='mean_squared_error')
-    model.fit(X, dataset[10:], epochs=50, batch_size=16, verbose=0)
+    model.fit(X, dataset[ts:], epochs=10, batch_size=16, verbose=0)
     return model, scaler, data
 
 def predict_next_prices(model, scaler, dataset):
     last_sequence = dataset[-24:].reshape(1, 24, 1)
     next_predictions = []
-    for _ in range(120):
+    for _ in range(24):
         next_price = model.predict(last_sequence, verbose=0)
         next_predictions.append(next_price[0, 0])
         next_sequence = np.array([[next_price[0, 0]]])
@@ -103,7 +90,7 @@ def historical_plot():
     df = data.copy()
     if 'error' in data:
         return None
-    data=create_features(data)
+    # data=create_features(data)
     model, scaler, data = training_model(data)
     
     next_predictions = predict_next_prices(model, scaler, data['Close'].values)  
@@ -119,7 +106,7 @@ def historical_plot():
         name='Historical Prices',
     ))
 
-    future_dates = pd.date_range(start=data['Datetime'].iloc[-1] + pd.Timedelta(hours=1), periods=120, freq='1h')
+    future_dates = pd.date_range(start=data['Datetime'].iloc[-1] + pd.Timedelta(hours=1), periods=24, freq='1h')
 
 
     # next_predictions=[61523.45703125, 61535.1171875, 61546.578125, 61558.66015625, 61573.515625, 61584.98828125, 61595.36328125, 61610.4140625, 61620.92578125, 61631.8203125, 61643.625, 61655.5234375, 61667.359375, 61679.14453125, 61690.88671875, 61702.5, 61714.1171875, 61725.765625, 61737.29296875, 61748.85546875, 61760.42578125, 61771.9765625, 61783.4921875, 61794.98828125, 61806.46484375, 61817.91796875, 61829.36328125, 61840.79296875, 61852.203125, 61863.6015625, 61874.984375, 61886.35546875, 61897.7109375, 61909.0546875, 61920.3828125, 61931.703125]
@@ -136,18 +123,14 @@ def index():
         symbol = request.form['symbol']
 
         
-        # Create Prophet plot asynchronously
-        # future_prophet = executor.submit(create_prophet_plot, symbol)
-        
+         
         future_prophet = executor.submit(create_prophet_plot, symbol)
-        
-        # Wait for results
+
         prophet_graphJSON = future_prophet.result()
         if not prophet_graphJSON:
             
             return render_template('index.html', prophet_graphJSON=None,error='error')
         
-        # Wait for results
         # prophet_graphJSON = create_prophet_plot(symbol)
 
         return render_template('index.html', symbol=symbol, prophet_graphJSON=prophet_graphJSON)
